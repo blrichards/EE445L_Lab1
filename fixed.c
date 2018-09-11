db08321698f95a16a9b13b074c5de0a8ca2f57f2
Ben Richards <blrichards@utexas.edu>
2018-09-10 22:28:17 -0500

    More cleanup and added function descriptions

--- fixed.c
+++ fixed.c
@@ -16,19 +16,35 @@
 #include "fixed.h"
 #include "ST7735.h"
 
+#define LCD_PLOT_MIN_X 0
+#define LCD_PLOT_MAX_X 127
+#define LCD_PLOT_MIN_Y 32
+#define LCD_PLOT_MAX_Y 159
+#define LCD_PLOT_HEIGHT (LCD_PLOT_MAX_Y - LCD_PLOT_MIN_Y)
+#define LCD_PLOT_WIDTH (LCD_PLOT_MAX_X - LCD_PLOT_MIN_X)
+
 int32_t xMin = 0;
 int32_t xMax = 0;
 int32_t yMin = 0;
 int32_t yMax = 0;
 
+/**
+ * Convert a number between 0 and 9 to its ASCII representation
+ * param: digit: Number to convert to ASCII
+ * return: ASCII Representation of digit param.
+ */
 static inline __attribute__((always_inline)) char digitToASCII(uint8_t digit) {
   return digit + 0x30;
 }
 
-/* NAME:
- * DESCRIPTION:	
- * INPUT:
- * OUTPUT:
+
+/**
+ * Output signed fixed point value to the display with a resolution of 0.01.
+ * param: value: Value to be printed to display.
+ * limitation: 
+ *   The value param must be between -10,000 and 10,000 (exclusive).
+ *   Otherwise, "***.**" will be printed to the display instead of 
+ *   the correct result.
  */
 void ST7735_sDecOut2(int32_t value) {
   char buf[6];
@@ -74,10 +90,12 @@ void ST7735_sDecOut2(int32_t value) {
 
 }
 
-/* NAME:
- * DESCRIPTION:	
- * INPUT:
- * OUTPUT:
+/**
+ * Output unsigned integer value to the display with a resolution of 1/64.
+ * param: value: Value to be printed to display.
+ * limitation: 
+ *   The value param must be less than 64,000, otherwise, "***.**" will
+ *   be printed to the display instead of the correct result.
  */
 void ST7735_uBinOut6(uint32_t value) {
   uint8_t outSize = 6;
@@ -106,10 +124,15 @@ void ST7735_uBinOut6(uint32_t value) {
   printf("%s", buf);
 }
 
-/* NAME: ST7735_XYplotInit
- * DESCRIPTION:	
- * INPUT:
- * OUTPUT:
+/**
+ * Initialize an XY plot with the given dimensions by first clearing the screen.
+ * param: title: Title of plot to be printed to the top of the display.
+ * param: minX: Minimum x coordinate of the plot.
+ * param: maxX: Maximum x coordinate of the plot.
+ * param: minY: Minimum y coordinate of the plot.
+ * param: maxY: Maximum y coordinate of the plot.
+ * limitation:
+ *   Assumes that minX < maxX and minY is less than maxY.
  */
 void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, int32_t maxY) {
   ST7735_FillScreen(0);
@@ -121,17 +144,20 @@ void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, in
   yMax = maxY;
 }
 
-/* NAME:
- * DESCRIPTION:	
- * INPUT:
- * OUTPUT:
+/**
+ * Initialize an XY plot with the given dimensions by first clearing the screen.
+ * param: title: Title of plot to be printed to the top of the display.
+ * param: minX: Minimum x coordinate of the plot.
+ * param: maxX: Maximum x coordinate of the plot.
+ * param: minY: Minimum y coordinate of the plot.
+ * param: maxY: Maximum y coordinate of the plot.
  */
 void ST7735_XYplot(uint32_t num, int32_t bufX[], int32_t bufY[]) {
   for (int i = 0; i < num; i++) {
     if (bufX[i] >= xMin && bufX[i] <= xMax && bufY[i] >= yMin && bufY[i] <= yMax) {
-      ST7735_DrawPixel((127 * (bufX[i] - xMin) / (xMax - xMin)), (32 + 127 * (yMax - bufY[i]) / (yMax - yMin)),
-                       ST7735_WHITE);
-			
+			int xCoordinate = (LCD_PLOT_WIDTH * (bufX[i] - xMin)) / (xMax - xMin);
+			int yCoordinate = ((LCD_PLOT_HEIGHT * (yMax - bufY[i])) / (yMax - yMin)) + LCD_PLOT_MIN_Y;
+      ST7735_DrawPixel(xCoordinate, yCoordinate, ST7735_WHITE);
 		}
   }
 }
--- fixed.h
+++ fixed.h
@@ -9,69 +9,46 @@
 
 #include <stdint.h>
 
-/****************ST7735_sDecOut2***************
- converts fixed point number to LCD
- format signed 32-bit with resolution 0.01
- range -99.99 to +99.99
- Inputs:  signed 32-bit integer part of fixed-point number
- Outputs: none
- send exactly 6 characters to the LCD 
-Parameter LCD display
- 12345    " **.**"
-  2345    " 23.45"  
- -8100    "-81.00"
-  -102    " -1.02" 
-    31    "  0.31" 
--12345    "-**.**"
- */ 
+/**
+ * Output signed fixed point value to the display with a resolution of 0.01.
+ * param: value: Value to be printed to display.
+ * limitation: 
+ *   The value param must be between -10,000 and 10,000 (exclusive).
+ *   Otherwise, "***.**" will be printed to the display instead of 
+ *   the correct result.
+ */
 void ST7735_sDecOut2(int32_t value);
 
 
-/**************ST7735_uBinOut6***************
- unsigned 32-bit binary fixed-point with a resolution of 1/64. 
- The full-scale range is from 0 to 999.99. 
- If the integer part is larger than 63999, it signifies an error. 
- The ST7735_uBinOut6 function takes an unsigned 32-bit integer part 
- of the binary fixed-point number and outputs the fixed-point value on the LCD
- Inputs:  unsigned 32-bit integer part of binary fixed-point number
- Outputs: none
- send exactly 6 characters to the LCD 
-Parameter LCD display
-     0	  "  0.00"
-     1	  "  0.01"
-    16    "  0.25"
-    25	  "  0.39"
-   125	  "  1.95"
-   128	  "  2.00"
-  1250	  " 19.53"
-  7500	  "117.19"
- 63999	  "999.99"
- 64000	  "***.**"
-*/
+/**
+ * Output unsigned integer value to the display with a resolution of 1/64.
+ * param: value: Value to be printed to display.
+ * limitation: 
+ *   The value param must be less than 64,000, otherwise, "***.**" will
+ *   be printed to the display instead of the correct result.
+ */
 void ST7735_uBinOut6(uint32_t value); 
 
-/**************ST7735_XYplotInit***************
- Specify the X and Y axes for an x-y scatter plot
- Draw the title and clear the plot area
- Inputs:  title  ASCII string to label the plot, null-termination
-          minX   smallest X data value allowed, resolution= 0.001
-          maxX   largest X data value allowed, resolution= 0.001
-          minY   smallest Y data value allowed, resolution= 0.001
-          maxY   largest Y data value allowed, resolution= 0.001
- Outputs: none
- assumes minX < maxX, and miny < maxY
-*/
+/**
+ * Initialize an XY plot with the given dimensions by first clearing the screen.
+ * param: title: Title of plot to be printed to the top of the display.
+ * param: minX: Minimum x coordinate of the plot.
+ * param: maxX: Maximum x coordinate of the plot.
+ * param: minY: Minimum y coordinate of the plot.
+ * param: maxY: Maximum y coordinate of the plot.
+ * limitation:
+ *   Assumes that minX < maxX and minY is less than maxY.
+ */
 void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, int32_t maxY);
 
-/**************ST7735_XYplot***************
- Plot an array of (x,y) data
- Inputs:  num    number of data points in the two arrays
-          bufX   array of 32-bit fixed-point data, resolution= 0.001
-          bufY   array of 32-bit fixed-point data, resolution= 0.001
- Outputs: none
- assumes ST7735_XYplotInit has been previously called
- neglect any points outside the minX maxY minY maxY bounds
-*/
+/**
+ * Initialize an XY plot with the given dimensions by first clearing the screen.
+ * param: title: Title of plot to be printed to the top of the display.
+ * param: minX: Minimum x coordinate of the plot.
+ * param: maxX: Maximum x coordinate of the plot.
+ * param: minY: Minimum y coordinate of the plot.
+ * param: maxY: Maximum y coordinate of the plot.
+ */
 void ST7735_XYplot(uint32_t num, int32_t bufX[], int32_t bufY[]);
 
 
